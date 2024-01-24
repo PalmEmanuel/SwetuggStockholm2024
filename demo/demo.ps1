887f193b8f139ef744b22d73f4677f23c7231974
Emanuel Palm <emanuel+github@pipe.how>
2024-01-24 22:52:24 +0100

    wip presentation

--- demo/demo.ps1
+++ demo/demo.ps1
@@ -1,3 +1,15 @@
-# Start Codespace
+# Pre-session
 
-# Connect to Codespace from VS Code
+## Create Codespace
+
+# Demo
+
+## Start Codespace
+## Connect to Codespace from VS Code
+
+```
+rad install kubernetes
+rad init
+> No
+rad run demo/app.bicep
+```
